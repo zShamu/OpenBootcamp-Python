d2873b2a4f8619b73fec58984d3004aa9dc9ea2a
zShamu <shamuelvargas@gmail.com>
2022-07-23 23:45:15 -0500

    E2 M5

---
+++ 5. funciones/ejercicio2.py
@@ -0,0 +1,16 @@
+numero = int(input("Introduce un numero: "))
+
+def esPrimo(numero):
+    if numero == 4:
+        return False
+    elif numero == 0 or numero == 1:
+        return True
+    for x in range(2, int(numero/2)):
+        if numero % x == 0:
+            return False
+    return True
+
+if esPrimo(numero):
+	print("Es primo")
+else:
+	print("No es primo")
