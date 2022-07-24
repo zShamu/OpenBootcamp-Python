37af3bf7e2e733e61bbe481bbb3c4229eaab770a
zShamu <shamuelvargas@gmail.com>
2022-07-24 01:43:31 -0500

    E1 M7

---
+++ 7. scripts/ejercicio1/main.py
@@ -0,0 +1,10 @@
+from mates import *
+
+def main():
+    print(suma(2, 2))
+    print(resta(2, 2))
+    print(multi(2, 2))
+    print(divi(2, 2))
+
+if __name__ == '__main__':
+    main()
---
+++ 7. scripts/ejercicio1/mates.py
@@ -0,0 +1,11 @@
+def suma(x, y) -> float:
+    return x + y
+
+def resta(x, y) -> float:
+    return x - y
+
+def multi(x, y) -> float:
+    return x * y
+
+def divi(x, y) -> float:
+    return x / y
